57a7b952f77d26f52853d3341e3bc6235a2e42ca
Maël Villerbu <mael.villerbu@gmail.com>
2022-11-24 21:09:16 +0000

    Handle disconnection/reconnection

--- scale.py
+++ scale.py
@@ -1,6 +1,7 @@
 import socket
 import RPi.GPIO as GPIO
 from hx711 import HX711
+import struct
 
 hx = HX711(24,23)
 hx.set_reading_format("MSB","MSB")
@@ -14,18 +15,28 @@ hostMACAddress = 'B8:27:EB:A1:CD:D7' # The MAC address of a Bluetooth adapter on
 port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
 backlog = 1
 size = 1024
+
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.bind((hostMACAddress,port))
-s.listen(backlog)
-try:
-    client, address = s.accept()
-    while 1:
-        data = client.recv(size)
-        if data:
-            weight = hx.get_weight(5)
-            client.send(bytes(str(weight), 'UTF-8'))
-    
-except:	
-    print("Closing socket")	
-    client.close()
-    s.close()
+while True: 
+    s.bind((hostMACAddress,port))
+    s.listen(backlog)
+    while True:
+        client, address = s.accept()
+        print("Connectiontion from", address)
+        try:
+            while True:
+                data = client.recv(size)
+                if data:
+                    command = data.decode('UTF-8')
+                    if command == 'data':
+                        weight = hx.get_weight(5)
+                        client.send(bytes(str(weight), 'UTF-8'))        
+                    else:
+                        print("Received unknown command")         
+        except (OSError, socket.error) as e:
+            print("Error:", e)
+            client.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', 1, 0))
+            client.close()
+        else:
+            client.shutdown(socket.SHUT_RDWR)
+            client.close()
